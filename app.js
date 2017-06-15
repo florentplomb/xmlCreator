ccbab1167786af7375687d4dcabe10937f9ddf4b
Florent Plomb <florent.plomb@avanade.com>
2017-06-15 17:44:46 +0200

    add bower path

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ app.get('/', function(request, response) {
     response.sendFile(__dirname + '/app');
 });
   app.use('/', express.static(__dirname + '/app'));
-
+app.use('/bower_components', express.static(__dirname + '/bower_components'));
 
 app.listen(port, function () {
   console.log('Example app listening on port 3000!');
